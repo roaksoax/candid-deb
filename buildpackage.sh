3fe966be78d357cb8c80d7ad02fabafc1d5c6e84
Jay R. Wren <jrwren@xmtp.net>
2015-04-03 16:12:09 -0400

    build binary package
    run make deps before building release package

--- buildpackage.sh
+++ buildpackage.sh
@@ -58,18 +58,18 @@ fi
 # Get the short hash of the most recent commit for versioning.
 SHORTHASH=$(git log -1 --pretty=format:%h)
 
-# Get all the dependent go source.
-make deps
-
-go test github.com/CanonicalLtd/blues-identity/...
-
-cd $GOPATH
-
 # Update the debian/changelog file.
 VERSION=${VERSION}+$(date -u +%Y%m%d%H%M)-${SHORTHASH}
 dch -v ${VERSION} 'autobuild: new commit' -D trusty
 
 fi
+
+# Get all the dependent go source.
+make -C $GOPATH/src/github.com/CanonicalLtd/blues-identity deps
+
+go test github.com/CanonicalLtd/blues-identity/...
+
+cd $GOPATH
 
 GIT_COMMIT=`git --git-dir ${GOPATH}/src/github.com/CanonicalLtd/blues-identity/.git rev-parse --verify HEAD`
 PKG=github.com/CanonicalLtd/blues-identity
@@ -77,7 +77,8 @@ VERSION_PKG=${PKG}/version
 gofmt -r "unknownVersion -> Version{GitCommit: \"${GIT_COMMIT}\", Version: \"${VERSION}\",}" ${GOPATH}/src/${VERSION_PKG}/init.go.tmpl > ${GOPATH}/src/${VERSION_PKG}/init.go
 
 # Build the source package.
-dpkg-buildpackage -rfakeroot -d -S -us -uc
+dpkg-buildpackage -rfakeroot -F -us -uc
+dpkg-buildpackage -rfakeroot -S -us -uc
 
 CHANGESFILE=$(./get_changes_filename.pl)
 
